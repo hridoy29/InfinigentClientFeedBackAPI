4a4b9d0d55f2f74bd291f385004a83041d78405f
Khalid <42520040+KhalidRupam@users.noreply.github.com>
2024-04-06 02:01:01 +0600

    Add cors

--- Infinigent_ClientFeednback/App_Start/WebApiConfig.cs
+++ Infinigent_ClientFeednback/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using System.Xml;
@@ -13,22 +14,19 @@ namespace Infinigent_ClientFeednback
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
-            // Web API routes
-
-            // Configure JSON serialization settings
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
 
             // Enable CORS
             var cors = new EnableCorsAttribute("*", "*", "*");
             config.EnableCors(cors);
 
-
+            // Web API routes
             config.MapHttpAttributeRoutes();
-
+            json.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
